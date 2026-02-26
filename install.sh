0624308fa8e8cfaddbbdc4b31b0d623abc954676
Paula Paysan <paula.paysan@gmail.com>
2026-02-26 15:30:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
-# ensure you set the executable bit on the file with `chmod u+x install.sh`
-
-# If you remove the .example extension from the file, once your workspace is created and the contents of this
-# repo are copied into it, this script will execute.  This will happen in place of the default behavior of the workspace system,
-# which is to symlink the dotfiles copied from this repo to the home directory in the workspace.
-#
-# Why would one use this file in stead of relying upon the default behavior?
-#
-# Using this file gives you a bit more control over what happens.
-# If you want to do something complex in your workspace setup, you can do that here.
-# Also, you can use this file to automatically install a certain tool in your workspace, such as vim.
-#
-# Just in case you still want the default behavior of symlinking the dotfiles to the root,
-# we've included a block of code below for your convenience that does just that.
-
 set -euo pipefail
 
 DOTFILES_PATH="$HOME/dotfiles"
@@ -29,5 +14,5 @@ done
 sudo apt-get update && sudo apt-get install -y cowsay
 sudo apt-get install -y fzf
 
-# Install fzf keybindings + completion
-/usr/share/doc/fzf/examples/install --key-bindings --completion --no-update-rc
+# Install fzf keybindings + completion (if available)
+[ -f /usr/share/doc/fzf/examples/install ] && /usr/share/doc/fzf/examples/install --key-bindings --completion --no-update-rc || true
